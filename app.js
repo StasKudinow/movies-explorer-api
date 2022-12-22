31a46a494fa45c3c42f7747531eddfaf08326b46
Stas Kudinov <dedbanzai91@mail.ru>
2022-12-22 18:58:45 +0300

    add limiter & install dotenv, helmet

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
 const { errors } = require('celebrate');
 
 const router = require('./routes/index');
 const { errorsHandler } = require('./middlewares/errors-handler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { DB_ADDRESS } = require('./utils/constants');
+const limiter = require('./middlewares/rate-limiter');
 
 const { PORT = 3000 } = process.env;
+
 const app = express();
+
+app.use(helmet());
 
 mongoose.set('strictQuery', true);
 mongoose.connect(DB_ADDRESS, {
@@ -18,6 +24,8 @@ mongoose.connect(DB_ADDRESS, {
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(limiter);
 
 app.use(requestLogger);
 
