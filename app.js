75739630f5b6c9a5820bd91ab3225dda22149112
Stas Kudinov <dedbanzai91@mail.ru>
2022-12-22 19:37:25 +0300

    change port 1.1

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { DB_ADDRESS } = require('./utils/constants');
 const limiter = require('./middlewares/rate-limiter');
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 
 const app = express();
 
@@ -41,5 +41,5 @@ app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('Сервер запущен на:');
   // eslint-disable-next-line no-console
-  console.log('http://localhost:3001');
+  console.log('http://localhost:3000');
 });
